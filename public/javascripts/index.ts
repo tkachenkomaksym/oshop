b7880a38642f5258cf67cd10fd6e8e6557cafa4b
Maksym Tkachenko <shelestidavai@gmail.com>
2019-06-21 12:29:35 +0300

    add fetch and display on products

---
+++ public/javascripts/index.ts
@@ -0,0 +1,46 @@
+
+console.log('Hello World');
+
+fetch('/products')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(products) {
+    console.log(products);
+  });
+
+interface Product {
+  name: string;
+  price: number;
+  currency: string;
+  photo: string;
+  description: string;
+}
+
+// function renederProducts(products: Product[]){
+//   const container = document.querySelector("#container");
+//
+//   products.forEach((product) => {
+//     const element = createProductElement(product);
+//     container.appendChild(container);
+//   });
+// }
+//
+// function createProductElement(product: Product){
+//   const product = document.createElement("div");
+//   const image = document.createElement("img");
+//   const name = document.createElement("span");
+//   const price = document.createElement("span");
+//   const description = document.createElement("p");
+//   const order = document.createElement("button");
+//
+//   image.src = product.photo;
+//   name.innerText = product.name;
+//   price.innerText = product.price + ' ' + product.currency;
+//
+//   product.appendChild(image)
+//
+//   return product;
+//
+// }
+
